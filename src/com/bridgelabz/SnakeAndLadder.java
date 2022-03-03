89a5f04841c82b00e5b27a8986abaf5e992e28b2
Chougulenik <nikitachougule47@gmail.com>
2022-03-03 16:30:42 -0500

    UC-1 added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 
+	int startPosition = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Snake And Ladder Game");
