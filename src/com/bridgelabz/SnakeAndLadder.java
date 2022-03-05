6c013ed36a5cee1cdd056242dae0a836cb9a3057
Chougulenik <nikitachougule47@gmail.com>
2022-03-05 15:58:07 -0500

    UC5 added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,23 +2,21 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class SnakeAndLadder {
-		
-	
 	static Random random = new Random();
 	static int getRandomValue(){
-			int randomValue = random.nextInt(6) +1;
+			int randomValue = random.nextInt(6);
 			System.out.println("Player Roll the die");
 			randomValue = randomValue + 1;
-			System.out.println("number : " +randomValue);
+			System.out.println("dice : " +randomValue);
 			return randomValue;
 	}
-	static int getRandomOption(int startPosition ,int dice) {
+	
+	static int getRandomOption(int startPosition, int dice) {
 			int randomOption = random.nextInt(3);
 			randomOption = randomOption + 1;
 			System.out.println("Choose option : " +randomOption);
 			
-			switch(randomOption)
-			{
+			switch(randomOption) {
 			case 0 :
 				   System.out.println("no play");
 				   break;
@@ -32,7 +30,6 @@ public class SnakeAndLadder {
 						System.out.println("Snake the player moves behind : " + startPosition);
 						break;
 			default :
-				
 			}
 			System.out.println();
 			return startPosition;
@@ -42,12 +39,19 @@ public class SnakeAndLadder {
 		
 			int startPosition = 0;
 			int dice =0;
-			System.out.println("Welcome to Snake And Ladder Game");
-			while(startPosition <= 100)	{
+			System.out.println("Welcome to Snake And Ladder Game \n");
+			System.out.println("Singal Player at start position " +startPosition);
+			while (startPosition <= 100)	{
 				dice = getRandomValue();
-				getRandomOption(startPosition , dice);
+				startPosition=getRandomOption(startPosition , dice);
 			}
-			System.out.println("Win the game postion is : " +startPosition);
+			if (startPosition > 100) {
+				int temp = startPosition - 100;
+				startPosition = startPosition - temp;
+				System.out.println("win the game position : "+startPosition);
+			}
+			else
+				System.out.println("Win the game postion : " +startPosition);
 
       }
       
