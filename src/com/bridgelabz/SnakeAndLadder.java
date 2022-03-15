46441187b2cf41e5b25420aaa0f4a7e649f62247
Chougulenik <nikitachougule47@gmail.com>
2022-03-14 22:52:21 -0400

    UC-7 added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -37,32 +37,71 @@ public class SnakeAndLadder {
 	
 		public static void main(String[] args) {
 		
-			int startPosition = 0;
+			int startPosition1 = 0;
+			int startPosition2 = 0;
 			int dice ;
-			int countDice = 0;
-			int countPos = 0;
+			int countDice1 = 0;
+			int countPos1 = 0;
+			int countDice2 = 0;
+			int countPos2 = 0;
 
 			System.out.println("Welcome to Snake And Ladder Game \n");
-			System.out.println("Singal Player at start position " +startPosition);
-			while (startPosition <= 100)	{
-				dice = getRandomValue();
-				countDice ++;
-				int t = startPosition;
-				startPosition=getRandomOption(startPosition , dice);
-				if(startPosition > t)
-					countPos ++;
+			System.out.println("Player-1 at start position " +startPosition1);
+			System.out.println("Player-2 at start position " +startPosition2);
+			
+			while (startPosition1 <= 100 || startPosition2 <= 100)	{
+				Random random = new Random();
+				int play = random.nextInt(2);
+				play = play +1;
+				if (play == 1) {
+					dice = getRandomValue();
+					countDice1 ++;
+					int t = startPosition1;
+					startPosition1=getRandomOption(startPosition1 , dice);
+					if(startPosition1 > t)
+						countPos1 ++;
+					if(startPosition1 > 100)
+						break;
+				}
+				else {
+					dice = getRandomValue();
+					countDice2 ++;
+					int t = startPosition2;
+					startPosition2=getRandomOption(startPosition2 , dice);
+					if(startPosition2 > t)
+						countPos2 ++;
+					if(startPosition2 > 100)
+						break;
+				}
 			}
-			if (startPosition > 100) {
-				int temp = startPosition - 100;
-				startPosition = startPosition - temp;
-				System.out.println("win the game position : "+startPosition);
-				System.out.println("No of count dice : "+countDice);
-				System.out.println("no of count position : "+countPos);
+			if (startPosition1 > 100 || startPosition2 > 100) {
+				int temp1 = startPosition1 - 100;
+				startPosition1 = startPosition1 - temp1;
+				int temp2 = startPosition2 - 100;
+				startPosition2 = startPosition2 - temp2;
+				
+				if (startPosition1 == 100 ) {
+					System.out.println("Player-1 win the game position : "+startPosition1);
+					System.out.println("Player-1 No of count dice : "+countDice1);
+					System.out.println("Player-1 no of count position : "+countPos1);
+				}
+				else if (startPosition2 == 100 ) {
+					System.out.println("Player-2 win the game position : "+startPosition2);
+					System.out.println("Player-2 No of count dice : "+countDice2);
+					System.out.println("Player-2 no of count position : "+countPos2);
+				}
 			}
 			else {	
-				System.out.println("Win the game postion : " +startPosition);
-				System.out.println("no of count dice : "+countDice);
-				System.out.println("no of count position : "+countPos);
+				if (startPosition1 == 100) {
+					System.out.println("Player-1 Win the game postion : " +startPosition1);
+					System.out.println("Player-1 no of count dice : "+countDice1);
+					System.out.println("Player-1 no of count position : "+countPos1);
+				}
+				else {
+					System.out.println("Player-2 Win the game postion : " +startPosition2);
+					System.out.println("Player-2 no of count dice : "+countDice2);
+					System.out.println("Player-2 no of count position : "+countPos2);
+				}	
 			}	
       }
       
